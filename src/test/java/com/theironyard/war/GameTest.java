10478b737ed3f4a74e17108f22aac262875efefa
Eduardo Jimenez <EddyJ@Eduardos-MacBook-Pro.local>
2016-06-28 01:16:30 -0700

    updated game with tests, working on last test

--- src/test/java/com/theironyard/war/GameTest.java
+++ src/test/java/com/theironyard/war/GameTest.java
@@ -1,5 +1,6 @@
 package com.theironyard.war;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,11 +13,23 @@ import static org.junit.Assert.*;
  */
 public class GameTest {
 
-    protected void setUp(){
+    @Test
+    public void whenSetUpHandsThenHandEquals26(){
         Player player1 = new Player("TestPlayer1");
         Player player2 = new Player("TestPlayer2");
+
+        assertTrue("Player1's hand should equal 26", player1.hand.size() == 0);
+        assertTrue("Player2's hand should equal 26", player2.hand.size() == 0);
     }
+    @Test
+    public void whenRunGameThenDeclareWinner(){
+        Player player1 = new Player("TestPlayer1");
+        Player player2 = new Player("TestPlayer2");
 
+        assertTrue("player1 should be declared winner if other player is out of cards", player2.hand.size() == 0);
+        assertTrue("player2 should be winner if other player is out of cards", player1.hand.size() == 0);
 
+        
+    }
 
 }
